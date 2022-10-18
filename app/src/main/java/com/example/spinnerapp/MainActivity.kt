f658060921b52cd4205b3f8cc74d3ce9bc6c1e1c
blackwing <hh4654075hh@gmail.com>
2022-10-18 20:31:36 +0800

    增加按鈕及事件

--- app/src/main/java/com/example/spinnerapp/MainActivity.kt
+++ app/src/main/java/com/example/spinnerapp/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.spinnerapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Spinner
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val spGender = findViewById<Spinner>(R.id.spGender)
+        val btOK = findViewById<Button>(R.id.btOK)
+        btOK.setOnClickListener {
+            Toast.makeText(this,"${spGender.selectedItem.toString()}",Toast.LENGTH_LONG).show()
+        }
     }
 }
